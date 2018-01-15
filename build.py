b722f2c5b3f6c402b5554ab50ed78a01513f8a69
Ulrich Eck <ueck@net-labs.de>
2018-01-15 15:56:05 +0100

    incorrect use of settings object in build.py

--- build.py
+++ build.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     builder.builds = [
         [settings, options, env_vars, build_requires]
         for settings, options, env_vars, build_requires in builder.builds
-        if (not (os_info.is_linux and settings["arch"] == "x86")) and options["shared"] == True 
+        if (not (os_info.is_linux and settings["arch"] == "x86")) and options.shared == True 
     ]
     builder.run()
