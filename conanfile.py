0770f91f52e8b14156a1c70b9db062d3c4c1c162
NARVIS-UECK\ueck <ulrich.eck@tum.de>
2018-03-11 18:30:34 +0100

    fix debug build on windows

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,5 @@
 from conans import ConanFile, CMake, tools
-from conans.tools import download, unzip, replace_in_file
+from conans.tools import download, unzip, replace_in_file, patch
 import os
 
 
@@ -16,6 +16,7 @@ class AssimpConan(ConanFile):
     options = {"shared": [True, False]}
     default_options = "shared=False"
     generators = "cmake"
+    exports = ["patches/*",]
 
     def source(self):
         zip_name = "v%s.zip" % self.version
@@ -23,11 +24,21 @@ class AssimpConan(ConanFile):
         unzip(zip_name)
         os.unlink(zip_name)
         os.rename("assimp-%s" % self.version, "source")
+
+        # if self.settings.os == "Windows" and self.settings.build_type=="Debug":
+        #     #patch assimp to correctly build debug libraries on windows
+        #     self.output.info("Apply patch: patch_windows_debug_build.diff")
+        #     patch(base_path="source", 
+        #         patch_file=os.path.join("patches", "patch_windows_debug_build.diff"), 
+        #         strip=1)
+
+
         # This small hack might be useful to guarantee proper /MT /MD linkage in MSVC
         # if the packaged project doesn't have variables to set it properly
         tools.replace_in_file("source/CMakeLists.txt", "PROJECT( Assimp )", '''PROJECT( Assimp )
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()''')
+
 
     def build(self):
         cmake = CMake(self)
@@ -49,14 +60,16 @@ conan_basic_setup()''')
         self.copy("*.inl", dst="include", src="source/include")
         self.copy("*assimp*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
         is_apple = (self.settings.os == 'Macos' or self.settings.os == 'iOS')
-        if self.settings.build_type == "Debug" and not is_apple:
-            self.cpp_info.libs = [lib+'d' for lib in self.cpp_info.libs]
+        # if self.settings.build_type == "Debug" and not is_apple:
+        #     self.cpp_info.libs = [lib+'d' for lib in self.cpp_info.libs]
         
         if self.settings.os == "Windows":
             self.cpp_info.cppflags.append("/EHsc")
