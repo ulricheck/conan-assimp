7603c3b89b3d0ad40d2dfa1e7052afba73dfa72a
NARVIS-UECK\ueck <ulrich.eck@tum.de>
2018-03-11 22:52:40 +0100

    try to fix build error on travis

--- conanfile.py
+++ conanfile.py
@@ -59,11 +59,13 @@ conan_basic_setup()''')
         self.copy("*.hpp", dst="include", src="source/include")
         self.copy("*.inl", dst="include", src="source/include")
         self.copy("*assimp*.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
+        if self.settings.os == "Windows":
+            self.copy("*.dll", dst="bin", keep_path=False)
+        if self.settings.os == "Macos":           
+            self.copy("*.dylib", dst="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
